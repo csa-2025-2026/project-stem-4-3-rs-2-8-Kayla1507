f1854ce0640e6a41937ca64f6f0f923156ce5ae1
per5 <klapham007@gmail.com>
2025-10-23 21:37:25 +0000

    done

--- Main.java
+++ Main.java
@@ -5,5 +5,45 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+
+    //3
+    /*System.out.println("enter a num between 0-50");
+    int num = sc.nextInt();
+
+    if (num <=0 || num >= 50)
+    {
+      System.out.println("error");
+      
+    }
+    else
+    {
+      for (int count = num; count <= 50; count++)
+      {
+        System.out.print(count + " ");
+        
+        if (count % 5 == (num+4) % 5)
+        {
+          System.out.println();
+          
+        }
+      }
+    }*/
+    int n = sc.nextInt();
+     if (n > 0)
+    {
+  
+      n -= (n % 3);	
+      for (int i = n; i >= 0; i -= 3)
+      {
+        System.out.print(i + " ");
+      }
+    }
+    else
+    {
+      System.out.println("error");
+    }
+          
   }
+
 }
